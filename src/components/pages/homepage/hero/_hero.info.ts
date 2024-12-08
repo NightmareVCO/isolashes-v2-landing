2b3a2b71108b4c810e8c879c9ba9bc679762cbf9
NightmareVCO <vladimircuriel@outlook.com>
2024-12-08 00:23:31 -0400

    feat(hero): add hero section with title, subtitle, and call-to-action button

---
+++ src/components/pages/homepage/hero/_hero.info.ts
@@ -0,0 +1,14 @@
+export type HeroInfo = typeof heroInfo;
+
+export const heroInfo = {
+  title: "Salón de Pestañas",
+  subtitle: "#1 Líder En Pestañas",
+  description: "",
+
+  titleHero: "SI HABLAS DE PESTAÑAS",
+  subtitleHero: "Hablas de Nosotras",
+  buttonText: "Agendar Cita",
+  buttonHref: "https://isolashes.setmore.com",
+};
+
+export default heroInfo;
