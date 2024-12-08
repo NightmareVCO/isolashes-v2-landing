f9d19836f88057388c0c334cfdf57333b1486968
NightmareVCO <vladimircuriel@outlook.com>
2024-12-08 00:26:07 -0400

    feat(appointment): add appointment section with form inputs and scheduling button

---
+++ src/components/pages/homepage/appointment/_appointment.info.ts
@@ -0,0 +1,11 @@
+export type AppointmentInfo = typeof appointmentInfo;
+
+export const appointmentInfo = {
+  normalText: "Agendar",
+  styledText: "Cita",
+  subtitle: "",
+  description:
+    "Agenda tu cita con nosotros y disfruta de nuestros servicios de la mejor calidad.",
+  buttonText: "Agendar Cita",
+  buttonHref: "/citas",
+};
