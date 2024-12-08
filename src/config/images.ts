b86ea26d18cb09a8240420124366d2d1a807168f
NightmareVCO <vladimircuriel@outlook.com>
2024-12-08 00:21:24 -0400

    feat(images): add image configuration for homepage, product, about, and services pages

---
+++ src/config/images.ts
@@ -0,0 +1,60 @@
+// how should be "https://drive.google.com/uc?export=view&id=1GX6KEcV128RzpkwOmK3p8GBR9QF7qYxb",
+
+// how will arrive https:drive.google.com/file/d/1EhfZg69NS_OG9SbjHXk4vcQgOuiHv64q/view?usp=drive_link
+export const homePageImages = {
+  hero: "https://drive.google.com/uc?export=view&id=1EhfZg69NS_OG9SbjHXk4vcQgOuiHv64q",
+  about1:
+    "https://drive.google.com/uc?export=view&id=15w9gSCyuiJavyj9OrYYoIKruv8J4Z-29",
+  about2:
+    "https://drive.google.com/uc?export=view&id=1puw2tgy2d2GzFSk97XTcVLxbdTd55mph",
+  pestaña1:
+    "https://drive.google.com/uc?export=view&id=16-FahKmzpY7aiIbHLV-3f7dtbAS-KaT-",
+  pestaña2:
+    "https://drive.google.com/uc?export=view&id=14oaWWV0iZvEADvQAmnfwEC3MlkMCgQFo",
+  cejas:
+    "https://drive.google.com/uc?export=view&id=1PzGkaq1FkAIJZrIY8Ra9SwBG1PGNHP4J",
+  labios:
+    "https://drive.google.com/uc?export=view&id=1-8_zIbHn8-PxR5VWRyLOCChI6VmV36Ve",
+  academia:
+    "https://drive.google.com/uc?export=view&id=1GX6KEcV128RzpkwOmK3p8GBR9QF7qYxb",
+  personal1:
+    "https://drive.google.com/uc?export=view&id=1s1trJiV3xbje-sdzoo5IS6W4R3wv7-ua",
+  cita: "https://drive.google.com/uc?export=view&id=1_0qyIc4BOgkhCsludt6eLFK9MR-RwC5P",
+};
+
+export const productImages = {
+  store_cover1:
+    "https://drive.google.com/uc?export=view&id=1Giy8UzXXEpsc2E1EsZcsOuQHx1IiTzQV",
+  store_cover2:
+    "https://drive.google.com/uc?export=view&id=1DJjmJSV8N7Gj34NwGKNfhIjk-VZjZUvK",
+  store_cover3:
+    "https://drive.google.com/uc?export=view&id=1tr8w-0sJZFZOM7WTq5HxJ358CKN_IHuY",
+  campaign1:
+    "https://drive.google.com/uc?export=view&id=1O1ScObbtDsYK7QD0zWDWkqvx6HoTO0X5",
+  pinza1:
+    "https://drive.google.com/uc?export=view&id=1MuaMwWO6SVMdkZD_RRv0MALb19GnJtjP",
+  pinza2:
+    "https://drive.google.com/uc?export=view&id=1noEwKbMF76N2ZhsdhyXbS4j2nlbv9sHk",
+  blister1:
+    "https://drive.google.com/uc?export=view&id=1StcSdRKNcNGlFi6vVYGw18uPFuQfQ50a",
+  blister2:
+    "https://drive.google.com/uc?export=view&id=10DK9ZZ95k8b7gN39E1kQPRaLXM5MLO3N",
+  adhesivo1:
+    "https://drive.google.com/uc?export=view&id=1jaOdBXP8IziPjLg0Ew4sW2FZ5Hw_TsnT",
+  adhesivo2:
+    "https://drive.google.com/uc?export=view&id=1gPI5NEGK5ZO6jsJBtY80Uur543B2Xu8n",
+};
+
+export const aboutPageImages = {
+  local:
+    "https://drive.google.com/uc?export=view&id=1fM72Xjv85pyor-PC3kO8OwCK3OfEJexl",
+  localDentro:
+    "https://drive.google.com/uc?export=view&id=1lkzos0sI1EQHaOX6f3IGka9lcKT8sTZY",
+  congreso:
+    "https://drive.google.com/uc?export=view&id=1EMeRu9JbK3hzHUeu8fKsJXROxl0Ry_yD",
+};
+
+export const servicesPageImages = {
+  decoration:
+    "https://drive.google.com/uc?export=view&id=1FCZNv4zaZavlibO1jZBzMD8HmqWmclBo",
+};
