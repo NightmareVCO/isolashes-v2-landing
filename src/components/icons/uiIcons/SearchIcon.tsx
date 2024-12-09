0af34b653384b1836d7c2ebe9e15751a84b29b6c
NightmareVCO <vladimircuriel@outlook.com>
2024-12-09 00:12:13 -0400

    feat(icons): add new SVG icons and remove legacy components

--- src/components/icons/uiIcons/SearchIcon.tsx
+++ src/components/icons/uiIcons/SearchIcon.tsx
@@ -1,43 +1,29 @@
-export interface SearchIconProperties {
-  size?: number;
-  strokeWidth?: number;
-  width?: number;
-  height?: number;
-}
-
-export default function SearchIcon({
-  size = 24,
-  strokeWidth = 2,
-  width,
-  height,
-  ...properties
-}: SearchIconProperties) {
+export default function SearchIcon() {
   return (
     <svg
-      className="transition hover:scale-110"
       aria-hidden="true"
+      className="transition hover:scale-110"
       fill="none"
       focusable="false"
-      height={height || size}
+      height={25}
+      width={25}
       role="presentation"
       viewBox="0 0 24 24"
-      width={width || size}
-      {...properties}
     >
       <path
         d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={strokeWidth}
-      />
+        strokeWidth={2}
+      ></path>
       <path
         d="M22 22L20 20"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth={strokeWidth}
-      />
+        strokeWidth={2}
+      ></path>
     </svg>
   );
 }
